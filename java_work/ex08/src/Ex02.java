f283ba805be275b500d065136d25ea9f3ad220f1
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-07-08 10:33:41 +0900

    0708

---
+++ java_work/ex08/src/Ex02.java
@@ -0,0 +1,12 @@
+// 2. Math.random을 이용해서 1부터 6사이의 임의의 정수를 변수 value에 저장하는 코드를 완성하여라
+//        (1)에 알맞은 코드를 넣으세요
+
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+//        int value = Math.random();
+//
+//        System.out.println("value = "+value);
+    }
+}
