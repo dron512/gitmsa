14e1b687e0a18da2351729d1941eeb75c3eb9a32
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-07-10 13:06:32 +0900

    0710

--- java_work/ex10/src/bank/BackAccountMain.java
+++ java_work/ex10/src/bank/BackAccountMain.java
@@ -8,8 +8,12 @@ public class BackAccountMain {
     public static void main(String[] args) {
         System.out.println("시작");
 
-        BankAccount park = new BankAccount("박길동");
-        BankAccount yoon = new BankAccount("윤길동");
+        // constructor
+        BankAccount park = new BankAccount("박길동","1234","850511",5000);
+        BankAccount yoon = new BankAccount("윤길동","4567","950511",5000);
+
+//        park.initAccount("1234","850511",5000);
+//        yoon.initAccount("4567","950511",5000);
 
         park.cmb();
         yoon.cmb();
