ed8f320969594299362a0faf1f2974827c455655
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-07-19 17:33:04 +0900

    0719

--- html_work/15/ex01.js
+++ html_work/15/ex01.js
@@ -1,5 +1,3 @@
-// let 변경가능한 변수 
-// const 변경불가능한 상수 
 const aa = document.getElementById("btn-min");
 const bb = document.getElementById("btn-max");
 const printDiv = document.getElementById("print-div");
@@ -10,31 +8,36 @@ const n3 = document.getElementById("num3");
 const n4 = document.getElementById("num4");
 const n5 = document.getElementById("num5");
 
-function min(){
-    // console.log(n1.value);
-    const arr = [n1.value,n2.value,n3.value,n4.value,n5.value];
-    console.log(arr);
+function min() {
+    const arr = [n1.value, n2.value, n3.value, n4.value, n5.value];
+    let min = arr[0];
     for (let index = 0; index < arr.length; index++) {
         const element = arr[index];
-        console.log(element);
-        
+        if (Number(min) > Number(element))
+            min = element;
     }
+    printDiv.textContent = "최솟값 = " + min;
 }
-function max(){
-    alert('max함수 호출');    
+function max() {
+    const arr = [n1.value, n2.value, n3.value, n4.value, n5.value];
+    let max = arr[0];
+    for (const key of arr) {
+        if (Number(max) < Number(key))
+            max = key;
+    }
+    printDiv.textContent = "최댓값 = " + max;
 }
 
-aa.addEventListener('click',min);
-bb.addEventListener('click',max);
+aa.addEventListener('click', min);
+bb.addEventListener('click', max);
 
-printDiv.textContent = "안녕하세요 내용을 넣는 거예요...";
 
 
 
 
 
 const cc = 10;
-let dd =10;
+let dd = 10;
 
 // 이거 에러... 못바꿈
 // cc = 20;
@@ -43,6 +46,6 @@ dd = 30;
 
 // console.log(dd);
 
-for (let i=0; i<10; i++){
+for (let i = 0; i < 10; i++) {
     // console.log(i);
 }
