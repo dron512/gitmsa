741bcae624830889e38de19da7bf4df0849e269c
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-07-23 10:24:12 +0900

    240723

---
+++ java_work/ex19/src/main/java/org/example/inter/Printable.java
@@ -0,0 +1,12 @@
+package org.example.inter;
+
+public interface Printable {
+
+    // static final 변수를 대문자로 짓는게 관례...
+
+    public static final int HEIGHT = 70;
+    int WIDTH = 120;
+
+    void print(String mydoc);
+
+}
