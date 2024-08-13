521412796dc5516017d618bdc54d839eef942705
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-08-13 12:21:09 +0900

    240813

--- java_work/ex20/src/main/java/org/example/Ex06.java
+++ java_work/ex20/src/main/java/org/example/Ex06.java
@@ -12,11 +12,14 @@ public class Ex06 {
         try{
             // DB연결 유지 클래스
             Connection conn
-                    = DriverManager.getConnection("jdbc:mysql://192.168.0.29:3307/aaaaa","root","1234");
+                    = DriverManager.getConnection("jdbc:mysql://192.168.0.29:3307/pmh","root","1234");
             System.out.println("되냐");
 
+            // SQL 구문 담는 객체
             PreparedStatement pstmt
-                    = conn.prepareStatement("INSERT INTO student values ('김길동','100','100','100')");
+                    = conn.prepareStatement("INSERT INTO student values ('김길동',100,100,100)");
+
+            pstmt.executeUpdate();
         }catch (Exception e){
             e.printStackTrace();
         }
