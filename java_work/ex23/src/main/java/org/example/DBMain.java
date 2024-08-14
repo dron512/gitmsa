f10a0d7182bae744901b1df433c08796869bc939
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-08-14 17:50:16 +0900

    240814

--- java_work/ex23/src/main/java/org/example/DBMain.java
+++ java_work/ex23/src/main/java/org/example/DBMain.java
@@ -3,12 +3,9 @@ package org.example;
 import java.util.Scanner;
 
 public class DBMain {
-
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        
+        DBRepository dbRepository = new DBRepository();
         while(true){
             System.out.println("""
                     뭐할래?
@@ -21,11 +18,11 @@ public class DBMain {
             int cho = scanner.nextInt();
             //insert 문 실행
             if(cho==1){
-                
+                dbRepository.insert();
             } 
             //select 문 실행
             else if (cho==2) {
-                
+                dbRepository.select();
             }
             else{
                 System.out.println("종료됩니다.");
@@ -33,6 +30,4 @@ public class DBMain {
             }
         }
     }
-
-
 }
