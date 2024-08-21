a05618a87a6017e3b8b6cdc2146993a00581dc13
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-08-21 10:16:58 +0900

    0821

--- js_work/01/ex01.js
+++ js_work/01/ex01.js
@@ -7,7 +7,10 @@ var btn = document.querySelector("#btn");
 // heading.onclick = doA;
 
 heading.onclick = function(){ 
-    heading.style.color = "red";
+    if(heading.style.color==='red')
+        heading.style.color = "blue";
+    else
+        heading.style.color = "red";
     heading.innerHTML = "글자도 바꿈";
     heading.style.backgroundColor="blue";
     heading.style.textAlign="center";
@@ -17,6 +20,8 @@ heading.onclick = function(){
 btn.onclick = function(){
     var name = prompt('당신의 이름은?','이름');
     alert(name);
+
+    // java javascript
     // fetch('localhost:8080/index.jsp')
     // .then
 }
