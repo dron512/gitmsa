a4ede4ae07df8c4b9c04af348cfcb2f9a9e7f550
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-08-22 11:34:06 +0900

    0822

---
+++ vue_work/vue-basic/src/components/WelcomeItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex text-indigo-600">
+    <h1 class="text-red-500">TEST</h1>
+    <input type="text" name="" id="" class="focus:outline-none border border-gray-600 focus:ring-1">
+  </div>
+</template>
+
+<style scoped>
+</style>
