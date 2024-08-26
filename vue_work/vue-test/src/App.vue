cf0364a72d94dbb3ce0846c59f724be9e2eb71a6
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-08-26 15:08:32 +0900

    0826

---
+++ vue_work/vue-test/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="flex space-x-10 p-3">
+        <RouterLink to="/" class="p-3 bg-slate-600 hover:bg-slate-500">Home</RouterLink>
+        <RouterLink to="/about" class="p-3 bg-slate-600 hover:bg-slate-500">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ vue_work/vue-test/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import dayjs from 'dayjs';
+
+const now = dayjs();
+
+const now1 = now.add(1,'day');
+</script>
+
+<template>
+  <main>
+  <h1>TEST</h1>
+    <h1>now = {{ now }}</h1>
+    <h1>now1 = {{ now1 }}</h1>
+    <h1>{{now.format('YYYY/MM/DD')}}</h1>
+    <h1>{{ now.startOf('month').format('YYYY/MM/DD') }}</h1>
+  </main>
+</template>
