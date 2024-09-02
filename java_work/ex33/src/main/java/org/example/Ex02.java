ce6bfcc6a89afd468d07ec116624e8ddc2fdec86
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-09-02 13:19:56 +0900

    240902

--- java_work/ex33/src/main/java/org/example/Ex02.java
+++ java_work/ex33/src/main/java/org/example/Ex02.java
@@ -1,16 +1,17 @@
 package org.example;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 
 public class Ex02 {
 
     public static void main(String[] args) {
         // file 내용쓰기
         // int double int
-        try(DataOutputStream dos = new DataOutputStream(new FileOutputStream("mydata"))){
+        try(DataOutputStream dos =
+                    new DataOutputStream(
+                    new BufferedOutputStream(
+                    new FileOutputStream("mydata")))
+        ){
             dos.writeInt(4);
             dos.writeDouble(17.3);
             dos.writeInt(9);
@@ -20,7 +21,11 @@ public class Ex02 {
         }
         // file 내용 읽기
         // int double int
-        try(DataInputStream dis = new DataInputStream(new FileInputStream("mydata"))){
+        try(DataInputStream dis =
+                    new DataInputStream(
+                    new BufferedInputStream(
+                    new FileInputStream("mydata")))
+        ){
             int temp = dis.readInt();
             double temp2 = dis.readDouble();
             int temp3 = dis.readInt();
