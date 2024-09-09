70049ec22ffd4d9e6fca99efd14e5805a196ea76
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-09-09 16:59:32 +0900

    0909

--- vue_work/ex03/src/router/index.js
+++ vue_work/ex03/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import TheHome from '../views/TheHome.vue';
 import TheSelect from '../views/TheSelect.vue';
 import TheDelete from '../views/TheDelete.vue';
+import TheInsert from '../views/TheInsert.vue';
+import TheUpdate from '../views/TheUpdate.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +22,16 @@ const router = createRouter({
       path: '/delete',
       name: 'delete',
       component: TheDelete
+    },
+    {
+      path: '/insert',
+      name: 'insert',
+      component: TheInsert
+    },
+    {
+      path: '/update',
+      name: 'update',
+      component: TheUpdate
     }
   ]
 })
--- vue_work/ex03/src/views/TheHome.vue
+++ vue_work/ex03/src/views/TheHome.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
+  <div class="p-10">
     <h1 class="text-5xl">HOME</h1>
+    <p>ref 반응형함수를 사용해볼께요</p>
+    <h2>{{ count }}</h2>
+    <div class="flex space-x-3">
+      <button @click="doPls" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">증가</button>
+      <button @click="doMin" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">감소</button>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+    const doPls = () => {
+      count.value = count.value + 1;
+    }
+    const doMin = () => {
+      count.value = count.value - 1;
+    }
+    return { count, doPls, doMin }
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
