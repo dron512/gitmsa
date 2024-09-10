e39b8e4babaaa60b6be0d2b7c5318c2e3681807f
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-09-10 17:11:21 +0900

    0910

--- vue_work/ex04/src/views/HomeView.vue
+++ vue_work/ex04/src/views/HomeView.vue
@@ -1,20 +1,46 @@
 <template>
   <div>
-    <h1>HOME</h1>
-    <ul>
-      <li v-for="user in list" v-bind:key="user.idx">
-        idx = {{ user.idx }}
-        name = {{ user.name }}
-      </li>
-    </ul>
-    <button @click="select" 
-      class="bg-yellow-200 
-               p-3 m-3
-              hover:bg-yellow-100
-              border border-gray-300
-              ">불러오기</button>
+    <h1 class="text-5xl">HOME</h1>
+    <div class="p-5">
+      <table class="border border-gray-500 w-full">
+        <tr>
+          <th class="border border-gray-500">idx</th>
+          <th class="border border-gray-500">name</th>
+          <th class="border border-gray-500">age</th>
+          <th class="border border-gray-500">password</th>
+          <th class="border border-gray-500">wdate</th>
+        </tr>
+        <tr v-for="user in list" v-bind:key="user.idx">
+          <td class="border border-gray-500">{{ user.idx }}</td>
+          <td class="border border-gray-500">{{ user.name }}</td>
+          <td class="border border-gray-500">{{ user.age }}</td>
+          <td class="border border-gray-500">{{ user.password }}</td>
+          <td class="border border-gray-500">{{ user.wdate }}</td>
+        </tr>
+      </table>
+      <button @click="select" class="bg-yellow-200 p-3 m-3 hover:bg-yellow-100 border border-gray-300">불러오기</button>
+    </div>
     <div v-for="num in mylist" v-bind:key="num">
       num = {{ num }}
+    </div>
+    <div class="p-5">
+      name = <input class="border border-yellow-400 focus:ring-2
+                focus:ring-yellow-500 focus:outline-none rounded-lg p-3 w-64
+                text-yellow-900 placeholder-yellow-500 bg-yellow-100 transition-all duration-300 shadow-md
+                hover:bg-yellow-200 hover:shadow-lg" type="text" name="name" id="" v-model="name"><br>
+      age = <input class="border border-yellow-400 focus:ring-2
+                focus:ring-yellow-500 focus:outline-none rounded-lg p-3 w-64
+                text-yellow-900 placeholder-yellow-500 bg-yellow-100 transition-all duration-300 shadow-md
+                hover:bg-yellow-200 hover:shadow-lg" type="text" name="name" id="" v-model="age"><br>
+      password = <input class="border border-yellow-400 focus:ring-2
+                focus:ring-yellow-500 focus:outline-none rounded-lg p-3 w-64
+                text-yellow-900 placeholder-yellow-500 bg-yellow-100 transition-all duration-300 shadow-md
+                hover:bg-yellow-200 hover:shadow-lg" type="text" name="name" id="" v-model="password"><br>
+      wdate = <input class="border border-yellow-400 focus:ring-2
+                  focus:ring-yellow-500 focus:outline-none rounded-lg p-3 w-64
+                text-yellow-900 placeholder-yellow-500 bg-yellow-100 transition-all duration-300 shadow-md
+                hover:bg-yellow-200 hover:shadow-lg" type="text" name="name" id="" v-model="wdate"><br>
+      <button @click="insert" class="bg-yellow-200 p-3 m-3 hover:bg-yellow-100 border border-gray-300">입력</button>
     </div>
   </div>
 </template>
@@ -24,7 +50,7 @@ import { ref } from 'vue';
 
 export default {
   setup() {
-    const mylist = [10,20,30];
+    const mylist = [10, 20, 30];
     const list = ref([
       {
         "idx": 1,
@@ -33,28 +59,30 @@ export default {
         "email": "바꿔@naver.com",
         "password": "1234",
         "wdate": "2024-09-06T11:18:32.729596"
-      },
-      {
-        "idx": 2,
-        "name": "이게되네",
-        "age": 25,
-        "email": "asdf@yahoo.com",
-        "password": "qwer",
-        "wdate": "2024-09-06T11:32:44.718893"
-      },
-      {
-        "idx": 3,
-        "name": "홍길동",
-        "age": 100,
-        "email": "qwer@naver.com",
-        "password": "1234",
-        "wdate": "2024-09-06T11:18:32.729596"
       }]);
-    const select = ()=>{
-      list.value = [{idx:1, name:"강제로 바꿈"}];
+    const name = ref('');
+    const age = ref('');
+    const password = ref('');
+    const wdate = ref('');
+    const select = () => {
+      fetch(`http://localhost:8080/user/select`)
+        .then(res => {
+          return res.json();
+        })
+        .then(result => {
+          list.value = result;
+        })
+        .catch(e => {
+          console.log("예외가 발생했습니다");
+          console.log(e);
+        })
     }
-    // fetch(`192.168.0.29:8080/user/select`)
-    return { list, mylist, select }
+    const insert = () => {
+      console.log("name.value "+name.value);
+      console.log("age.value "+age.value);
+    }
+
+    return { list, mylist, select, insert, name, age, password, wdate }
   }
 }
 </script>
