593f9e18dc494324838a1ee8fee96770e036cfe6
DESKTOP-C2SAMM8\pmh <parkmyounghoi@gmail.com>
2024-09-11 10:21:07 +0900

    0911

---
+++ springboot_work/ex06/src/main/java/com/pmh/ex06/user/UserService.java
@@ -0,0 +1,14 @@
+package com.pmh.ex06.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    // email 중복이면 에러코드 .
+    // email 중복이 아니면 insert 정상적으로 입력...
+    public void insert(){
+
+    }
+}
