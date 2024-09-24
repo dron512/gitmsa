0ba1e39034de116744053be9768a701e0bfbd24e
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-09-24 15:25:49 +0900

    240924

--- springboot_work/ex10/src/main/java/com/pmh/ex10/file/FileController.java
+++ springboot_work/ex10/src/main/java/com/pmh/ex10/file/FileController.java
@@ -1,15 +1,38 @@
 package com.pmh.ex10.file;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("file")
 public class FileController {
 
+    private final Path imagePath;
+
+    public FileController() {
+        this.imagePath = Paths.get("ex10/images/file/").toAbsolutePath();
+
+        try{
+            Files.createDirectories(this.imagePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @GetMapping("test")
     public String test() {
         return "test";
+    }
+
+    @PostMapping("upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
+        return "upload";
     }
 }
--- vue_work/ex06/src/views/TheFileUpload.vue
+++ vue_work/ex06/src/views/TheFileUpload.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="h1-red">TheFileUpload</h1>
     <div class="p-5">
-      <input type="file" name="file" id="" />
+      <input type="file" name="file" id="" @change="onFileChange" />
     </div>
     <div class="p-5">
       <button
@@ -13,13 +13,26 @@
         전송
       </button>
     </div>
+    myfile = {{ myfile }}
   </div>
 </template>
 
 <script setup>
-const save = ()=>{
-    console.log("save");
-}
+// import axios from 'axios';
+import { ref } from 'vue';
+const myfile = ref(null);
+
+const save = () => {
+  if (!myfile.value) {
+    alert('파일을 선택하세요');
+    return;
+  }
+
+  // axios.post('http://localhost:10000/file/upload')
+};
+const onFileChange = (e) => {
+  myfile.value = e.target.value;
+};
 </script>
 
 <style lang="scss" scoped></style>
