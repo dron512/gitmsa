86c5869d822c7b105d58b525dcc6e81f4179314e
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-10-04 16:15:48 +0900

    241004

--- springboot_work/ex10/src/main/java/com/pmh/ex10/login/LoginFilter.java
+++ springboot_work/ex10/src/main/java/com/pmh/ex10/login/LoginFilter.java
@@ -1,4 +1,25 @@
 package com.pmh.ex10.login;
 
-public class LoginFilter {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+
+        return super.attemptAuthentication(request, response);
+    }
 }
