cd227142d844bc9108d6bd82ee6c782dc91cee34
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-10-18 13:12:50 +0900

    20241018

--- vue_work/user-todo-dayjs/src/views/LoginView.vue
+++ vue_work/user-todo-dayjs/src/views/LoginView.vue
@@ -1,19 +1,15 @@
 <template>
-	<div>
-		<h1>LOGIN</h1>
-		<div class="flex flex-col items-center gap-4 p-6 bg-yellow-100 rounded-lg shadow-lg">
-			<a id="custom-login-btn" @click="kakaoLogin()" class="transition-transform transform hover:scale-105">
+	<div class="">
+		<div class="flex flex-col items-center justify-center gap-4 p-6 rounded-lg shadow-lg min-height-80">
+			<div id="custom-login-btn" @click="kakaoLogin()" class="transition-transform transform hover:scale-105">
 				<img
 					src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
 					width="222"
 					alt="카카오 로그인 버튼"
 					class="rounded-lg shadow-md"
 				/>
-			</a>
-			<div
-				@click="kakaoLogout()"
-				class="px-4 py-2 text-sm text-white bg-red-500 rounded-full cursor-pointer hover:bg-red-600 transition-all"
-			>
+			</div>
+			<div @click="kakaoLogout()" class="px-4 py-2 text-sm text-white bg-red-500 rounded-full cursor-pointer hover:bg-red-600 transition-all">
 				로그아웃
 			</div>
 		</div>
@@ -21,7 +17,18 @@
 </template>
 
 <script setup>
-const kakaoLogin = () => {};
+const kakaoLogin = () => {
+	window.Kakao.Auth.authorize({
+		redirectUri: 'http://localhost:5173/oauth',
+	});
+};
+const kakaoLogout = () => {
+	window.Kakao.Auth.logout();
+	// localStorage.removeItem('kakao_access_token');
+	// localStorage.removeItem('kakao_refresh_token');
+	// localStorage.removeItem('kakao_id');
+	// alert('로그아웃 성공');
+};
 </script>
 
 <style lang="scss" scoped></style>
