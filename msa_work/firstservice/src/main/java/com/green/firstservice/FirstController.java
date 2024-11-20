4e5c8d42da1213c6f0090584b03dae0cf2a766a2
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-11-20 18:08:16 +0900

    241120

--- msa_work/firstservice/src/main/java/com/green/firstservice/FirstController.java
+++ msa_work/firstservice/src/main/java/com/green/firstservice/FirstController.java
@@ -1,5 +1,6 @@
 package com.green.firstservice;
 
+import com.green.firstservice.feign.SecondFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("first-service")
 public class FirstController {
     private final Environment environment;
+    private final SecondFeignClient secondFeignClient;
 
     @GetMapping("test")
     public String test(){
+        System.out.println(secondFeignClient.getTest());
         System.out.println(" 여기 서버가 동작...."+environment.getProperty("local.server.port"));
         return "FirstService";
     }
