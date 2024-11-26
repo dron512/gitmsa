5abf00675e4923a7ef0b0aa9838d40243ca4253e
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-11-26 12:58:54 +0900

    241126

--- msa_work/orderservice/src/main/java/com/green/orderservice/order/OrderController.java
+++ msa_work/orderservice/src/main/java/com/green/orderservice/order/OrderController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("order-service")
 @RequiredArgsConstructor
@@ -16,13 +18,18 @@ public class OrderController {
 
     // 해당하는 사용자 주문하기
     @PostMapping("/{userId}/order")
-    public ResponseEntity<OrderResponse> userOrder(@PathVariable Long userId,
+    public ResponseEntity<OrderResponse> userOrder(@PathVariable String userId,
                                                    @RequestBody OrderRequest orderRequest) {
-        OrderResponse orderResponse = orderService.order(userId, orderRequest);
+        OrderResponse orderResponse = orderService.order(orderRequest, userId);
         return ResponseEntity.ok(orderResponse);
     }
 
     // 해당하는 사용자 주문목록보기
+    @GetMapping("/{userId}/order")
+    public ResponseEntity<List<OrderResponse>> userOrders(@PathVariable String userId) {
+        List<OrderResponse> orderResponses = orderService.list(userId);
+        return ResponseEntity.ok(orderResponses);
+    }
 
 
 }
--- msa_work/orderservice/src/main/java/com/green/orderservice/order/jpa/OrderRepository.java
+++ msa_work/orderservice/src/main/java/com/green/orderservice/order/jpa/OrderRepository.java
@@ -2,11 +2,11 @@ package com.green.orderservice.order.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
 
-    // select * from users where order =?;
-    Optional<OrderEntity> findByUserId(String userId);
+    List<OrderEntity> findAllByUserId(String userId);
 
 }
